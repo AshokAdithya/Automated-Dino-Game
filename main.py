38ab3b8ba49da116b95766ec62b62465d09bd7ed
AshokAdithya <ashokadithya2006@gmail.com>
2024-02-06 23:36:30 +0530

    feat: Added feature

--- main.py
+++ main.py
@@ -1,1 +1,30 @@
 # Automated dino game using pyautogui in Dark Mode
+
+import pyautogui as pa
+from PIL import Image,ImageGrab
+import time
+
+def hit(key):
+    pa.keyDown(key)
+
+def collide(data):
+    for i in range(300,415):
+        for j in range(563,650):
+            if data[i,j]<100 and data[i,j]>10:
+                hit("up")
+                return 
+    for i in range(300,415):
+        for j in range(410,563):
+            if data[i,j]<100 and data[i,j]>10:
+                hit("down")
+                return
+    return 
+
+
+if __name__=="__main__":
+    time.sleep(3)
+    hit("up")
+    while True:
+        image=ImageGrab.grab().convert("L")
+        data=image.load()
+        collide(data)
